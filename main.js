253a65ad6f922e47041880bd17a8d95cc12206fe
Marshal Marks <marshmarks@gmail.com>
2026-02-11 05:13:35 -0600

    Added dynamic styling on navBar

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const header = document.getElementById('navBar');
-    const className = 'scrolled-to-top';
+document.addEventListener("DOMContentLoaded", (event) => {
+    const header = document.getElementById("navBar");
+    const className = "scrolled-to-top";
 
-    // Function to check scroll position and apply/remove class
+    // Check scroll position and add/remove class
     function checkScrollPosition() {
         console.log(window.scrollY)
-        if (window.scrollY === 0) {
-            // User is at the very top
-            if (!header.classList.contains(className)) {
+        if(window.scrollY === 0) {
+            // User is at the top of the page
+            if(!header.classList.contains(className)) {
                 header.classList.add(className);
             }
         } else {
-            // User is not at the top
+            // User is not at the top of the page
             if (header.classList.contains(className)) {
                 header.classList.remove(className);
             }
         }
     }
 
-    // Listen for the scroll event
     window.addEventListener('scroll', checkScrollPosition);
 
-    // Run once on page load to set the initial state (in case user reloads at the top)
     checkScrollPosition();
 });
